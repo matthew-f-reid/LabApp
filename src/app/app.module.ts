af40e16ed4e729af8343f541813130837579438e
Matthew Reid <Matthew.Reid@griffithuni.edu.au>
2021-05-12 20:51:43 +1000

    Dashboard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InductionComponent } from './induction/induction.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InductionComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
