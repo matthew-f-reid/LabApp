04ee15a0c98e7c7dfe180f2cc5c182024f63302a
Matthew Reid <Matthew.Reid@griffithuni.edu.au>
2021-05-13 19:51:22 +1000

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InductionComponent } from './induction/induction.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'induction', component: InductionComponent }
+  { path: "", redirectTo: '/dash', pathMatch: 'full' },
+  { path: 'induction', component: InductionComponent },
+  { path: 'dash', component: DashboardComponent },
 ];
 
 @NgModule({
